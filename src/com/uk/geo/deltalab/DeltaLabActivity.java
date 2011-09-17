36f9a8b08c64890f71bd7ed9034e0497af546e66
= <pete@daffodil.uk.com>
2011-09-17 20:48:23 +0100

    See what this end up like

---
+++ src/com/uk/geo/deltalab/DeltaLabActivity.java
@@ -0,0 +1,13 @@
+package com.uk.geo.deltalab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeltaLabActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
