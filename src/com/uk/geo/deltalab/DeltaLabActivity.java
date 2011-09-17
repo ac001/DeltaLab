33b400eb34c21ad3d0daa2dc4091b57390a29fc4
= <pete@daffodil.uk.com>
2011-09-17 21:50:43 +0100

    Toast

--- src/com/uk/geo/deltalab/DeltaLabActivity.java
+++ src/com/uk/geo/deltalab/DeltaLabActivity.java
@@ -2,12 +2,27 @@ package com.uk.geo.deltalab;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class DeltaLabActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+               
         setContentView(R.layout.main);
+        
+        final Button button = (Button) findViewById(R.id.b_login);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on clicks
+                Toast.makeText(DeltaLabActivity.this, "Beep Bop", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        
     }
 }
